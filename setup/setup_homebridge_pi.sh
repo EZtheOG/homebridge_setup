74528e0b3fb99b9f4c956f44df5d09e93a1bd32e
EZtheOG <ez@risingtidegames.com>
2017-05-21 23:18:18 -0400

    adding sh script to install homebridge

---
+++ setup/setup_homebridge_pi.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#
+# This script is to setup a raspberry pi from scratch as opposed to doing it manually
+#
+
+echo "starting install of packages"
+echo
+echo
+
+apt-get install git-core gcc build-essential git make libssl-dev vim -y
+wait
+
+echo "starting git clone"
+
+cd /opt
+git clone https://github.com/timleland/rfoutlet
+wait
+
+git clone git://git.drogon.net/wiringPi
+wait
+
+git clone https://github.com/EZtheOG/homebridge_setup
+wait
+
+git clone https://github.com/ManiacalLabs/BiblioPixel
+wait
+
+echo 
+echo
+echo "set hostname to homebridge"
+echo
+sed -i 's/raspberrypi/homebridge/g' /etc/hosts
+sed -i 's/raspberrypi/homebridge/g' /etc/hostname
+echo
+
+echo "installing nodejs"
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+wait
+apt-get install nodejs -y
+
+echo
+echo "installing GPIO via WiringPi"
+cd /opt/wiringPi
+./build
+wait
+
+useradd --system homebridge
+sleep .5
+
+echo "installing homebridge"
+echo
+declare -a npm_pkg=("homebridge" "hap-nodejs" "node-gyp" "homebridge-rcswitch-gpiomem" "homebridge-platform-lightify" "homebridge-cmdswitch2")
+
+for i in "${npm_pkg}"; do
+	npm install --global --unsafe-perm $i
+	wait
+done
+
+
+mkdir /etc/homebridge
+cp /opt/homebridge_setup/config/config.json /etc/homebridge/
+cp /opt/homebridge_setup/config/homebridge.init /etc/init.d/homebridge
+chown -R /etc/homebridge
+chmod -R 0700 /etc/homebridge
+chmod 755 /etc/init.d/homebridge
+sleep .5
+
+echo "homebridge is setup"
+echo 
+echo "done"
