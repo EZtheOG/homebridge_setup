d5e0e87b66603ecc1b7fef0e2f8d06f71f9d0377
EZtheOG <ez@risingtidegames.com>
2017-09-17 23:11:22 -0400

    Update setup_homebridge_pi.sh
    added comments and forgot to add update-rc.d to set homebridge and ssh to autostart

--- setup/setup_homebridge_pi.sh
+++ setup/setup_homebridge_pi.sh
@@ -3,15 +3,17 @@
 #
 # This script is to setup a raspberry pi from scratch as opposed to doing it manually
 #
+# Array of NPM Plugins
 declare -a npm_pkg=('homebridge' 'homebridge-rcswitch-gpiomem' 'homebridge-platform-lightify' 'homebridge-cmdswitch2' 'homebridge-sonos' 'homebridge-platform-wemo')
 
 echo "starting install of packages"
 echo
 echo
-
+# Apt Packages
 apt-get install git-core gcc build-essential git make libssl-dev vim libavahi-compat-libdnssd-dev -y
 wait
 
+# Git Repos for Rasp
 echo "starting git clone"
 
 cd /opt
@@ -24,6 +26,12 @@ wait
 git clone https://github.com/ManiacalLabs/BiblioPixel
 wait
 
+echo
+echo "installing GPIO via WiringPi"
+cd /opt/wiringPi
+./build
+wait
+
 echo 
 echo
 echo "set hostname to homebridge"
@@ -32,28 +40,29 @@ sed -i 's/raspberrypi/homebridge/g' /etc/hosts
 sed -i 's/raspberrypi/homebridge/g' /etc/hostname
 echo
 
+#Install NodeJS
 echo "installing nodejs"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 wait
 apt-get install nodejs -y
 
-echo
-echo "installing GPIO via WiringPi"
-cd /opt/wiringPi
-./build
-wait
-
+#Create user Homebridge
 useradd --system homebridge
 sleep .5
 
 echo "installing homebridge"
 echo
 
+# Install Homebridge Plugins
 for i in "${npm_pkg}"; do
 	npm install --global --unsafe-perm $i
 	wait
 done
 
+# Create directory structure
+# Copy Files
+# Chown + Chmod Homebridge dir
+# Homebridge user add to sudoers
 
 mkdir /etc/homebridge
 cp /opt/homebridge_setup/config/config.json /etc/homebridge/
@@ -64,6 +73,10 @@ chmod -R 0700 /etc/homebridge
 chmod 755 /etc/init.d/homebridge
 sleep .5
 
+# Last step is to add homebridge + sshd to autostart
+update-rc.d homebridge defaults
+update-rc.d ssh defaults
+update-rc.d ssh enable
 
 echo "homebridge is setup"
 echo 
